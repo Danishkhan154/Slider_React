8878dcdf66b19b263c54f3a197fc342d53207ffe
Danish Khan <danishkhankrz@gmail.com>
2021-09-04 21:25:57 +0530

    first commit

---
+++ src/components/SliderData.js
@@ -0,0 +1,26 @@
+export const SliderData = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1630688231126-dd36840fce51?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=401&q=80",
+  },
+
+  {
+    image:
+      "https://images.unsplash.com/photo-1630683392459-f316bea8e7ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+  },
+
+  {
+    image:
+      "https://images.unsplash.com/photo-1630659879694-b1750389d796?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=313&q=80",
+  },
+
+  {
+    image:
+      "https://images.unsplash.com/photo-1630567477760-46030b0c4c25?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=376&q=80",
+  },
+
+  {
+    image:
+      "https://images.unsplash.com/photo-1606787620651-3f8e15e00662?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+  },
+];
